addf3252a98ba3278fa4832edbc808807daece42
ablaz <a.blazhenkov@gmail.com>
2021-04-08 21:58:08 +0400

    1

---
+++ lection4.py
@@ -0,0 +1,5 @@
+games_count=int(input('how many games wanna play:'))
+print(f'Ok, ypu are about to play{games_count}games...')
+print('Please enter for Rock:0, Paper:1, Scissors:2')
+
+while games_count !=0
