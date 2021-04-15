534df938bd640e65f2f34d9dbfe672940ec5a5e4
Aleksey <82176844+rok-strongo@users.noreply.github.com>
2021-04-15 19:58:15 +0400

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,11 @@
+number_1=85
+number_2=176
+number_3=94
+
+if number_1==number_2:
+    print('yes')
+elif number_1==number_3:
+    print('yes')
+elif number_2==number_3:
+    print('yes')
+
---
+++ homework9.py
@@ -0,0 +1,5 @@
+test_str_1 = "  Hello world   this is   me   "
+test_str_2 = "Hello world   this is   me again   "
+test_str_3 = "      "
+test_str_4 = "   Hello world   this is  not actually me   :)"
+
