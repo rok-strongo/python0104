bd5c7c962758a0b2a7f3af86fa936ca45c0ab77e
Aleksey <82176844+rok-strongo@users.noreply.github.com>
2021-04-21 19:32:23 +0400

    Delete homework9.py

--- homework9.py
+++
@@ -1,5 +0,0 @@
-test_str_1 = "  Hello world   this is   me   "
-test_str_2 = "Hello world   this is   me again   "
-test_str_3 = "      "
-test_str_4 = "   Hello world   this is  not actually me   :)"
-
