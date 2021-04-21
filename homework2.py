17e00b0ef0cf4a631ab29a206b19d2d0c038dc2c
Aleksey <82176844+rok-strongo@users.noreply.github.com>
2021-04-21 19:31:47 +0400

    Delete homework2.py

--- homework2.py
+++
@@ -1,6 +0,0 @@
-user_input_1=int(input("input deposit amount:"))
-user_input_2=int(input("input annual percent:"))
-result=user_input_1+(user_input_1/100)*(user_input_2*5) # формула расчета суммы с процентами
-print(f"total ammount:{result}")
-
-
